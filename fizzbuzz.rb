66cbd10ec64a578f0e4cfdd56a500e53243562b1
Hilary Berigan <berigan.hilary@gmail.com>
2018-03-13 16:11:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,10 @@
-def fizzbuzz
-
+def fizzbuzz(number)
+    if number % 3 == 0 && number % 5 == 0
+      return "FizzBuzz"
+    elsif number % 3 == 0
+      return "Fizz"
+    elsif number % 5 == 0
+      return "Buzz"
+    end
 
 end
